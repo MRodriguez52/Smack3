bbfbc3edcc243ee9cb7845331d410119b632828c
Miguel Rodriguez <miguelrodriguez@miguels-macbook-pro-2.local>
2018-10-08 21:20:04 -0500

    Initial Commit

---
+++ Smack/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Smack
+//
+//  Created by Miguel Rodriguez on 10/8/18.
+//  Copyright © 2018 Miguel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
